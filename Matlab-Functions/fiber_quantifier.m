3423e6993f88fe5ac5b629ddc4ebef9b75fcb703
Bruce Damon <bruce.damon@vanderbilt.edu>
2020-09-08 12:07:31 -0500

    Update fiber_quantifier.m

--- Matlab-Functions/fiber_quantifier.m
+++ Matlab-Functions/fiber_quantifier.m
@@ -1,10 +1,10 @@
 function [angle_list, distance_list, curvature_list, fiber_all_mm, n_points, apo_area] = fiber_quantifier(fiber_all, roi_mesh, fq_options)
-%
-%FUNCTION fiber_quantifier
+% 
+% FUNCTION fiber_quantifier
 %  [angle_list, distance_list, curvature_list, fiber_all_mm, n_points, apo_area] = ...
 %    fiber_quantifier(fiber_all, roi_mesh, fq_options)
-%
-%USAGE
+% 
+% USAGE
 %    The function fiber_quantifier is used to calculate the muscle architectural
 %  parameters pennation angle, fiber tract length, and curvature in the 
 %  MuscleDTI_Toolbox. Calculations are only made for fiber tracts having 10 or 
@@ -13,19 +13,19 @@ function [angle_list, distance_list, curvature_list, fiber_all_mm, n_points, apo
 %  efficiency. The method for curvature measurements is described in Damon et al, 
 %  Magn Reson Imaging 2012. Best results are obtained with polynomial-fitted  
 %  fiber tracts, calculated using fiber_smoother.
-%
-%INPUT ARGUMENTS
+% 
+% INPUT ARGUMENTS
 %  fiber_all: A 4D matrix containing the fiber tract points, with units of
 %    pixels (in X and Y) or slice number (in Z).  
-%
+% 
 %  roi_mesh: The mesh reconstruction of the aponeurosis that was used as the
 %    seed surface for fiber tracking, output from define_roi.
-%
+% 
 %  fq_options: A user-defined structure containing the following fields:
 %    dwi_res: a three element vector with the FOV, (assumed to be the same for
-%      the x and y directions), in-plane matrix size, and the slice thickness.
-%      The FOV and slice thickness must be specified in mm.
-%
+%      the x and y directions), in-plane matrix size, and the slice thickness
+%       of the DTI images. The FOV and slice thickness must be specified in mm.
+% 
 %    filt_kernel: Pennation angle is calculated as complement to the angle
 %      formed by position vectors along the tract and the normal vector to
 %      the seed point of the tract. To do so, two lines tangent to the point
@@ -34,38 +34,38 @@ function [angle_list, distance_list, curvature_list, fiber_all_mm, n_points, apo
 %      a single odd integer, e.g. fq_options.filt_kernel=N.  Note that median 
 %      filtering results in a loss of edge information and that the number of 
 %      lost rows and columns increases with the size of the filter kernel.
-%
-%OUTPUT ARGUMENTS
+% 
+% OUTPUT ARGUMENTS
 %  angle_list: The pennation angles, for fiber tracking point numbers
 %    2-end. Pennation angles are reported in degrees.
-%
+% 
 %  distance_list: Distances along the fiber tracts, for fiber tracking point
 %    numbers 1-end. Distances are reported in mm.
-%
+% 
 %  curvature_list: The curvature values, for fiber tracking point numbers
 %    starting at 2 and ending 3 points before the tract's end. The latter is to 
 %    avoid abrupt changes in tract position.  Curvature values are reported in m-1.
-%
+% 
 %  fiber_all_mm: The fiber tract points converted to units of mm.
-%
+% 
 %  n_points: A 3D matrix (rows x columns x 3) containing the number of
 %    points used to quantify length, pennation, and curvature in each tract
-%
+% 
 %  apo_area: A 2D matrix (rows x columns) containing the amount of
 %    apoeurosis area represented by each fiber tract
-%
-%OTHER FUNCTIONS IN THE MUSCLE DTI FIBER-TRACKING TOOLBOX
+% 
+% OTHER FUNCTIONS IN THE MUSCLE DTI FIBER-TRACKING TOOLBOX
+%  For help visualizing the data, see <a href="matlab: help fiber_visualizer">fiber_visualizer</a>.
 %  For help defining the mask, see <a href="matlab: help define_muscle">define_muscle</a>.
 %  For help defining the ROI, see <a href="matlab: help define_roi">define_roi</a>.
 %  For help with the fiber tracking program, see <a href="matlab: help fiber_track">fiber_track</a>.
 %  For help smoothing fiber tracts, see <a href="matlab: help fiber_smoother">fiber_smoother</a>.
-%  For help selecting fiber tracts following their quantification, see <a href="matlab: help fiber_selector">fiber_selector</a>.
-%  For help visualizing the data, see <a href="matlab: help fiber_visualizer">fiber_visualizer</a>.
-%
-%VERSION INFORMATION
+%  For help selecting fiber tracts following their quantification, see <a href="matlab: help fiber_goodness">fiber_goodness</a>.
+% 
+% VERSION INFORMATION
 %  v 0.1
-%
-%ACKNOWLEDGEMENTS
+% 
+% ACKNOWLEDGEMENTS
 %  People: Zhaohua Ding, Adam Anderson, Anneriet Heemskerk
 %  Grant support: NIH/NIAMS R01 AR050101, NIH/NIAMS R01 AR073831
 
@@ -113,13 +113,13 @@ dx_dy = zeros(length(roi_mesh_mm(:,1,1))-1, length(roi_mesh_mm(1,:,1))-1);      
 dy_dz = zeros(length(roi_mesh_mm(:,1,1))-1, length(roi_mesh_mm(1,:,1))-1);                  %form a zeros matrix to hold dy/dz
 
 for row_cntr = 1:length(roi_mesh_mm(:,1,1))
-    inplane_x = squeeze(roi_mesh_mm(row_cntr,:, 1));                                        %find the x positions
-    inplane_y = squeeze(roi_mesh_mm(row_cntr,:, 2));                                        %find the y positions
+    inplane_x = squeeze(roi_mesh_mm(row_cntr,:, 2));                                        %find the x positions
+    inplane_y = squeeze(roi_mesh_mm(row_cntr,:, 1));                                        %find the y positions
     dx_dy(row_cntr,:) = diff(inplane_x)./diff(inplane_y);                                   %calculate approximate derivative as delta X / delta Y
 end
 
 for col_cntr = 1:length(roi_mesh_mm(1,:,1))
-    thruplane_y = squeeze(roi_mesh_mm(:, col_cntr, 2));                                     %find the y positions
+    thruplane_y = squeeze(roi_mesh_mm(:, col_cntr, 1));                                     %find the y positions
     thruplane_z = squeeze(roi_mesh_mm(:, col_cntr, 3));                                     %find the z positions
     dy_dz(:,col_cntr) = diff(thruplane_y)./diff(thruplane_z);                               %calculate approximate derivative as delta Y / delta Z
 end
@@ -144,20 +144,20 @@ for row_cntr = start_row:end_row
     for col_cntr = start_col:end_col
         
         %aponeurosis area calculations
-        v0=squeeze(roi_mesh_mm(row_cntr, col_cntr,1:3));                                    %v1-4 are the four vertices of the quadrilateral on the roi mesh
-        v1=squeeze(roi_mesh_mm(row_cntr+1, col_cntr,1:3));                                  %v1-4 are the four vertices of the quadrilateral on the roi mesh
-        v2=squeeze(roi_mesh_mm(row_cntr+1, col_cntr+1,1:3));                                %v1-4 are the four vertices of the quadrilateral on the roi mesh
-        v3=squeeze(roi_mesh_mm(row_cntr, col_cntr+1,1:3));                                  %v1-4 are the four vertices of the quadrilateral on the roi mesh
+        p0=squeeze(roi_mesh_mm(row_cntr, col_cntr,1:3));                                    %p0-3 are the four points of the quadrilateral on the roi mesh
+        p1=squeeze(roi_mesh_mm(row_cntr+1, col_cntr,1:3));                                  %p0-3 are the four points of the quadrilateral on the roi mesh
+        p2=squeeze(roi_mesh_mm(row_cntr+1, col_cntr+1,1:3));                                %p0-3 are the four points of the quadrilateral on the roi mesh
+        p3=squeeze(roi_mesh_mm(row_cntr, col_cntr+1,1:3));                                  %p0-3 are the four points of the quadrilateral on the roi mesh
         apo_area(row_cntr, col_cntr) = ...                                                  %model the quadrilateral as two irregular triangles with vertices v0-v1-v2 and v2-v3-v0
-            norm(cross((v1-v0), (v2-v1)))/2 + norm(cross((v3-v2), (v3-v0)))/2;              %  their area is 1/2 of the magnitude of the indicated cross products
+            norm(cross((p1-p0), (p2-p1)))/2 + norm(cross((p3-p2), (p3-p0)))/2;              %  their area is 1/2 of the magnitude of the indicated cross products
             
-        %find initial point
-        seed_point = squeeze(fiber_all_mm(row_cntr, col_cntr, 1, :));
-        x0 = seed_point(1);
-        y0 = seed_point(2);
-        z0 = seed_point(3);
-        
-        if fiber_length(row_cntr,col_cntr) > 6                                           	%quantify all tracts of at least 6 points
+        if fiber_length(row_cntr,col_cntr) >= 6                                           	%quantify all tracts of at least 6 points
+
+            %find initial point
+            seed_point = squeeze(fiber_all_mm(row_cntr, col_cntr, 1, :));
+            x0 = seed_point(2);
+            y0 = seed_point(1);
+            z0 = seed_point(3);
             
             %get the tangent line in the in-plane direction from the matrix
             %calculated above
@@ -178,22 +178,22 @@ for row_cntr = start_row:end_row
             y2 = local_yz_intrcpt + local_dydz*z2;
             
             %calculate the Cartesian equation for the plane and the unit normal vector
-            A = seed_point;
-            B = [x1 y1 z1]';
-            C = [x2 y2 z2]';
-            dnB = B-A;
-            dnC = C-A;
-            normal_vector = cross(dnB, dnC);
+            point_0 = seed_point;
+            point_1 = [y1 x1 z1]';
+            point_2 = [y2 x2 z2]';
+            dP1 = point_1-point_0;
+            dP2 = point_2-point_0;
+            normal_vector = cross(dP1, dP2);
             unit_normal_vector = normal_vector/norm(normal_vector);
             
             %find the fiber length, pennation angle, and curvature as a function of position along the fiber:
             for fiber_cntr = 2:fiber_length(row_cntr,col_cntr)
                 
                 %fiber length
-                r_diff = squeeze(fiber_all_mm(row_cntr, col_cntr, fiber_cntr, :)) - ...
+                delta_p = squeeze(fiber_all_mm(row_cntr, col_cntr, fiber_cntr, :)) - ...
                     squeeze(fiber_all_mm(row_cntr,col_cntr, fiber_cntr-1, :));             	%form position vector from preceding point in the tract
                 distance_list(row_cntr,col_cntr,fiber_cntr)  =  ...                         %Euclidean distance from preceding point + the preceding point
-                    sqrt(sum(r_diff.^2)) + distance_list(row_cntr,col_cntr,fiber_cntr-1);
+                    sqrt(sum(delta_p.^2)) + distance_list(row_cntr,col_cntr,fiber_cntr-1);
                 
                 %pennation measurements for each point on the tract
                 r_vector = squeeze(fiber_all_mm(row_cntr, col_cntr, fiber_cntr, :)) - ...   %form position vectors along the tract
@@ -204,13 +204,12 @@ for row_cntr = start_row:end_row
                     theta_degrees = asind(dot(r_unit, -unit_normal_vector));
                 end
                 
-                
                 angle_list(row_cntr,col_cntr,fiber_cntr) = theta_degrees;                  	%write to matrix
                 if isnan(angle_list(row_cntr,col_cntr,fiber_cntr))                         	%in case of dividing by zero
                     angle_list(row_cntr,col_cntr) = 0;
                 end
                 
-                %keep track of nunmber of points quantified.
+                %keep track of number of points quantified.
                 n_points(row_cntr,col_cntr,1) = fiber_cntr;                                 %In 3rd dimension, save #points for distance data in level 1
                 n_points(row_cntr,col_cntr,2) = fiber_cntr-1;                               %In 3rd dimension, save #points for pennation data in level 2; 
                                                                                             %use fiber_cntr-1 because measurements start at point #2
@@ -223,15 +222,15 @@ for row_cntr = start_row:end_row
                     p3_idx = fiber_cntr+1;
                     loop_fiber_m = squeeze(fiber_all_mm(row_cntr,col_cntr,:,:))/1000;      	%convert from mm to m for curvature measurements
                     
-                    r_1 = (loop_fiber_m(p1_idx,:))-loop_fiber_m(1,:);                       %three position vectors, one for each point
-                    r_2 = (loop_fiber_m(p2_idx,:))-loop_fiber_m(1,:);
-                    r_3 = (loop_fiber_m(p3_idx,:))-loop_fiber_m(1,:);
+                    delta_p1 = (loop_fiber_m(p1_idx,:))-loop_fiber_m(1,:);                       %three position vectors, one for each point
+                    delta_p2 = (loop_fiber_m(p2_idx,:))-loop_fiber_m(1,:);
+                    delta_p3 = (loop_fiber_m(p3_idx,:))-loop_fiber_m(1,:);
                     
                     ds21 = sqrt(sum((loop_fiber_m(p2_idx,:)-loop_fiber_m(p1_idx,:)).^2));   %distance between points 1 and 2 and (below) 2 and 3
                     ds32 = sqrt(sum((loop_fiber_m(p3_idx,:)-loop_fiber_m(p2_idx,:)).^2));
                     
-                    tangent_vector_2 = (r_2-r_1)/norm(r_2-r_1);                           	%normalized tangent lines between the two pairs of points
-                    tangent_vector_3 = (r_3-r_2)/norm(r_3-r_2);
+                    tangent_vector_2 = (delta_p2-delta_p1)/norm(delta_p2-delta_p1);                           	%normalized tangent lines between the two pairs of points
+                    tangent_vector_3 = (delta_p3-delta_p2)/norm(delta_p3-delta_p2);
                     dTds = ((tangent_vector_3-tangent_vector_2)/mean([ds21 ds32]))';      	%dT/ds is the spatial rate of change in tangent lines
                     dTds(isnan(dTds)) = 0;
                     
